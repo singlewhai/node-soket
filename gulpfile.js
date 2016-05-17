7172d3e04d21abf2ee83118f3bc53071141de1e3
singlewhai <n.ratanasuwan@gmail.com>
2016-05-17 10:07:42 -0500

    chat

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,52 @@ var gulp    = require('gulp')
   , jshint  = require('gulp-jshint')
   , refresh = require('gulp-livereload')
  
-gulp.task('lint', function () {
-  gulp.src('./**/*.js')
-    .pipe(jshint())
+gulp.task('styles', function() {
+    return gulp.src('angular/styles/main.less')
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.autoprefixer({browsers: ['last 1 version']}))
+        .pipe(gulp.dest('.tmp/styles'));
 });
+
+gulp.task('jshint', function () {
+    return gulp.src('./**/*.js')
+        .pipe(jshint())
+});
+
+gulp.task('html', ['styles'], function() {
+    var cssChannel = lazypipe()
+        .pipe($.csso)
+        .pipe($.replace, 'bower_components/bootstrap/fonts', 'fonts');
+
+    var assets = $.useref.assets({searchPath: '{.tmp,app}'});
+
+    return gulp.src('app/**/*.html')
+        .pipe(assets)
+        .pipe($.if('*.js', $.ngAnnotate()))
+        .pipe($.if('*.js', $.uglify()))
+        .pipe($.if('*.css', cssChannel()))
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*')
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src(require('main-bower-files')().concat('angular/fonts/**/*')
+    .concat('bower_components/bootstrap/fonts/*'))
+    .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(gulp.dest('.tmp/fonts'));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 gulp.task('serve', function(){
     nodemon({script: 'server.js'})
@@ -18,15 +60,49 @@ gulp.task('serve', function(){
 
 });
 
-gulp.task('watch', ['lint', 'serve'], function(){
+gulp.task('watch', ['jshint', 'serve'], function(){
     gulp.watch([
         './**/*.js',
         './*.js'
-    ]).on('change', function(){
-        console.log('change');
-    });
+    ]).on('change', ['serve']);
+    gulp.watch([
+        './**/*.js',
+        './*.js'
+    ]).on('change', ['serve']);
+    gulp.watch('bower.json', ['wiredep']);
+    gulp.watch('angular/styles/**/*.less', ['styles']);
 });
 
-gulp.task('default', ['watch']);
+// inject bower components
+gulp.task('wiredep', function() {
+  var wiredep = require('wiredep').stream;
+  var exclude = [
+    /*'bootstrap',*/
+    'es5-shim',
+    'json3',
+    'angular-scenario'
+  ];
+
+  gulp.src('angular/styles/*.less')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app/styles'));
+
+  gulp.src('angular/*.html')
+    .pipe(wiredep({exclude: exclude}))
+    .pipe(gulp.dest('app'));
+});
+
+
+
+gulp.task('builddist', ['jshint', 'html', 'images', 'fonts'],
+  function() {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('build', ['clean'], function() {
+  gulp.start('builddist');
+});
+
+gulp.task('default', ['watch', 'wiredep']);
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const feathers          = require('feathers');
 const rest              = require('feathers-rest');
 const socket            = require('feathers-socketio');
+const path              = require('path');
 const bodyParser        = require('body-parser');
 const methodOverride    = require('method-override');
 const mongoose          = require('mongoose');
@@ -13,16 +14,54 @@ var app = feathers();
 // Connect to database
 mongoose.connect(config.database.uri, config.database.optiong);
 
-var socketCon = socket(function(io){
+var socketCon = socket(function(io){ 
+    io.use(function(socket, next){
+        //console.log("Query: ", socket.handshake);
+        // return the result of next() to accept the connection.
+        if (socket.handshake.query.appid == "123456789") {
+            return next();
+        }
+        // call next() with an Error if you need to reject the connection.
+        next(new Error('Authentication error'));
+    });
+    io.set('authorization', function (handshakeData, callback) {
+        console.log('Auth: ', handshakeData._query);
+        callback(null, true);
+    });
+    var clients = [];
     io.on('connection', function(socket){
         console.log('a user connected');
-        socket.broadcast.emit('hi');
+        clients.push(socket);
+        
+        function sendClientList(clients, currentId){
+            var clientList = [];
+            clients.forEach(function(e){
+                if(e.id === currentId)
+                    clientList.push('/#public');
+                else
+                    clientList.push(e.id); 
+            });
+            console.log(currentId, clientList);
+            io.to(currentId).emit('list', clientList);
+        }
+        socket.on('ready', function(id){
+            console.log(socket.id, id, 'id');
+            sendClientList(clients, socket.id);
+        });
+
         socket.on('disconnect', function(){
             console.log('user disconnected');
+            clients.splice(clients.indexOf(socket), 1);
+            clients.forEach(function(client, index) {
+                var client_id = index; // Just use the index in the clients array for now
+                sendClientList(clients, socket.id);
+            });
         });
         socket.on('chat message', function(msg){
             io.emit('chat message', msg);
-            console.log('message: ' + msg);
+        });
+        socket.on('private message', function(id, msg){
+            io.to(id).emit('private message', msg);
         });
     });
 });
@@ -36,8 +75,10 @@ app.configure(rest())
     .use(settings.cross());
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
+
+app.use(feathers.static(path.join(__dirname, 'bower_components')));
 
 var server = app.listen(config.server.port, function(){
     var host = server.address().address
